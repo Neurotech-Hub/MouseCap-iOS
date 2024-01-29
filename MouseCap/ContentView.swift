4794c49763dfdff964f9e711b23e1bd5daaf8907
Matt Gaidica, Ph.D <matt@gaidi.ca>
2024-01-29 13:41:02 -0600

    Initial Commit

---
+++ MouseCap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MouseCap
+//
+//  Created by Matt Gaidica on 1/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MouseCap/MouseCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseCapApp.swift
+//  MouseCap
+//
+//  Created by Matt Gaidica on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MouseCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
